de739cf7f41f0f7cfbab0a34fbb69c33ba1dda01
leeyehji <lamp0525@naver.com>
2024-06-04 09:49:15 +0900

    캘린더 수정 삭제 가능. 엔티티 allDay 추가.

---
+++ src/main/java/com/together/user_calendar/DAO/CalendarDAO.java
@@ -0,0 +1,28 @@
+package com.together.user_calendar.DAO;
+
+import com.together.board.bean.BoardDTO;
+import com.together.user_calendar.bean.User_calendarDTO;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+@Transactional
+public interface CalendarDAO extends JpaRepository<User_calendarDTO, Integer> {
+
+    //@Query("SELECT c FROM User_calendarDTO c WHERE c.calendar_memberId = :memberId AND FUNCTION('MONTH', c.calendar_start) = :month AND FUNCTION('YEAR', c.calendar_start) = :year")
+    @Query("SELECT c FROM User_calendarDTO c WHERE c.calendar_memberId = :memberId AND c.start < :endDate AND c.start >= :startDate")
+    List<User_calendarDTO> findByMonthAndMemberId(@Param("startDate") Date startDate, @Param("endDate") Date endDate, @Param("memberId") String memberId);
+
+    @Modifying
+    @Query("UPDATE User_calendarDTO c SET c.start = :start, c.end = :end, c.title = :title, c.content = :content, c.backgroundColor = :backgroundColor, c.borderColor = :backgroundColor, c.allDay = :allDay WHERE c.calendar_id = :calendarId")
+    void updateCalendar(@Param("calendarId") int calendarId, @Param("start")Date start, @Param("end")Date end, @Param("title")String title, @Param("content")String content, @Param("backgroundColor")String backgroundColor, @Param("allDay")boolean allDay);
+}
